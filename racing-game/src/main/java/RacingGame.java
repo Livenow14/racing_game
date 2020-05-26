ec4ad43c0bbd42f59aba2c6d5eb93ccadb14a641
Livenow14 <xowjd41@naver.com>
2020-05-27 00:36:39 +0900

    Initialize Commit

---
+++ racing-game/src/main/java/RacingGame.java
@@ -0,0 +1,10 @@
+import input.InputView;
+
+import java.util.Scanner;
+
+public class RacingGame {
+
+    public void run(){
+        InputView inputView = new InputView(new Scanner(System.in));
+    }
+}
---
+++ racing-game/src/main/java/input/InputView.java
@@ -0,0 +1,12 @@
+package input;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private Scanner scanner;
+
+    public InputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+}
